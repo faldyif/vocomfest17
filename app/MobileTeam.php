69cc1602b7d407fdc5bf641fc96fbc92ecb38cf3
faldyif <faldy.if@gmail.com>
2017-01-08 00:50:39 +0700

    Remove unnecessary function

--- app/MobileTeam.php
+++ app/MobileTeam.php
@@ -14,9 +14,4 @@ class MobileTeam extends Model
     protected $fillable = [
         'user_id', 'instance_name', 'instance_address', 'leader_name', 'leader_phone', 'member1_name', 'member1_email', 'member1_phone', 'member2_name', 'member2_email', 'member2_phone', 'member3_name', 'member3_email', 'member3_phone'
     ];
-
-    public function user()
-    {
-        return 0;
-    }
 }
