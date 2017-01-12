4bca061c3338dc6cd21712e7659da11754983a3b
faldyif <faldy.if@gmail.com>
2017-01-13 03:52:52 +0700

    Implement gallery initialization

---
+++ database/migrations/2017_01_12_204852_create_galleries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('status')->default(1); // 1 if published, 0 if unpublished
+            $table->string('path');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+}
