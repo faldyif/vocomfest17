a52f4f606f253e9b282efbff725127416b47e1f7
Faldy Ikhwan Fadila <Faldy Ikhwan Fadila>
2017-01-30 20:44:56 +0700

    Fix 500 error on semnas admin

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,10 +45,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if($e instanceof NotFoundHttpException)
-        {
-          return response()->view('errors.404', [], 404);
-        }
         return parent::render($request, $exception);
     }
 
